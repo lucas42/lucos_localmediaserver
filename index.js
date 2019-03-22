f5960e1fa2950c4e06308527ce8dcdb162552da7
Luke Blaney <git@lukeblaney.co.uk>
2019-03-21 23:41:41 -0400

    don't allow files just because there's a file extention in the query string

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 // Hacky middleware to only expose files ending in .mp3
 app.get('*', function(req, res, next){
 	res.setHeader("Access-Control-Allow-Origin", "*");
-	if (!req.url.endsWith(".mp3") && !req.url.endsWith(".ogg")) {
+	if (!req.path.endsWith(".mp3") && !req.path.endsWith(".ogg")) {
 		res.status(404).send("Not a recognised audio file");
 		return;
 	}
